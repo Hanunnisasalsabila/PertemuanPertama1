cef2bffa600bac596a8765ba9d15109dab109e23
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2024-03-04 16:03:52 +0700

    add variable jejari and luas

--- PertemuanPertama1/PertemuanPertama1.cpp
+++ PertemuanPertama1/PertemuanPertama1.cpp
@@ -3,6 +3,16 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+	//begin 
+	  // numeric nJejari,nLuas
+	  // display 'Masukan jejari= '
+	  // accept nJejari 
+	  // compute nLuas = 3.14 * r * r
+	  // display 'Luasnya= " + nLuas
+	 //end 
 
+	double nJejari, nLuas;
 
 
